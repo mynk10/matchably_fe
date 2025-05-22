a54b4ff534417301dccd4bca9d302b052f2e8b5b
mynk10 <mynk1008@gmail.com>
2025-05-22 11:42:11 +0530

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+import Body from "./components/Body";
+import Profile from "./components/Profile";
+import Login from "./components/Login";
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <Footer/>
-    </div>
+    <BrowserRouter basename="/">
+      <Routes>
+        <Route path="/" element={<Body />}>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/profile" element={<Profile />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+<div>
+  <Navbar />
+  <Footer />
+</div>;
